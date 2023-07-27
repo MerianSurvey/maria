f1748f373ec9db337f797f79c9519488689796d0
Erin Kado-Fong <kadofong@princeton.edu>
2023-07-27 12:33:58 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup (
+    name = 'maria',
+    version='0.0.1',
+    author='Erin Kado-Fong',
+    description='handy dandy merian catalog functions',
+    packages=setuptools.find_packages(),
+    #python_requires='>=3.7'
+    )
